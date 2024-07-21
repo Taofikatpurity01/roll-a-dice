5e076d008f2b8fc075fac20d110edf36c6558126
taofikatpurity01 <taofikatoyedepo@gmail.com>
2024-07-21 17:17:55 +0100

    roll-a-die

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//roll button
-image_array = [
+const image_array = [
     {
         value:1,
         image: 'die-1.png',
@@ -25,65 +24,83 @@ image_array = [
         image: 'die-6.png',
     },
 ]
-    // 'die-1.png',
-    // 'die-2.png',
-    // 'die-3.png',
-    // 'die-4.png',
-    // 'die-5.png',
-    // 'die-6.png']
 
 const rollDiceBtn = document.querySelector(".btn--roll")
+const dice = document.querySelector('.dice')
 const playerOneCurrentScore = document.querySelector("#current--0")
 const playerTwoCurrentScore = document.querySelector("#current--1")
 const playerOneScore = document.querySelector("#score--0")
 const playerTwoScore = document.querySelector("#score--1")
+const newGame = document.querySelector(".btn--new")
+const holdBtn = document.querySelector(".btn--hold")
+const playerOneSection = document.querySelector(".player--0")
+const playerTwoSection = document.querySelector(".player--1")
+
 let score__0 = 0
 let score__1 = 0
-localStorage.setItem("current-player", "player-1")
-const current_user = localStorage.getItem("current-player")
+let current_player = "player 1"
 
-rollDiceBtn.addEventListener("click", function(){
+
+// ================== ROLL DICE BUTTON EVENT LISTENER =======================
+
+rollDiceBtn.addEventListener("click", function() {
     const random_index = Math.floor(Math.random() * image_array.length);
      const selected_images = image_array[random_index]
-    //  console.log(selected_images)
-     document.querySelector('.dice').src = `./img/${selected_images.image}`
+     dice.src = `./img/${selected_images.image}`
  
-     if(current_user==="player-1"){
+     if(current_player === "player 1") {
         playerOneCurrentScore.innerText= selected_images.value
-        score__0 += selected_images.value
-        playerOneScore.innerText = score__0
-     }else if(current_user==="player-2") {
+        if(selected_images.value == 1) {
+            ChangeActivePlayer()
+        } else {
+            score__0 += selected_images.value
+            playerOneScore.innerText = score__0
+        }
+     }else {
         playerTwoCurrentScore.innerText= selected_images.value
-        score__1 += selected_images.value
-        playerTwoScore.innerText = score__1
+        if(selected_images.value == 1) {
+            ChangeActivePlayer()
+        } else {
+            score__1 += selected_images.value
+            playerTwoScore.innerText = score__1
+        }
      }
-    currentScore.innerText =selected_images.value
+
+     if(score__0 >= 100) {
+        alert(`${current_player} wins`)
+        window.location.reload()
+     } else if (score__1 >= 100) {
+        alert(`${current_player} wins`)
+        window.location.reload()
+     }
 })
 
 
-const newGame = document.querySelector(".btn--new")
-newGame.addEventListener("click", function(){
-    window.location.reload()
-})
+// ================== CHANGE ACTIVE PLAYER FUNCTION AND HOLD BUTTON EVENT LISTENER =======================
 
-
-const holdBtn = document.querySelector(".btn--hold")
-const playerOne= document.querySelector(".player--0")
-const playerTwo= document.querySelector(".player--1")
+function ChangeActivePlayer() {
+    if(current_player === "player 1") {
+        current_player = "player 2"
+        playerOneScore.innerText = score__0
+        playerTwoSection.classList.add("player--active")
+        playerOneSection.classList.remove("player--active")
+    }else {
+        current_player = "player 1"
+        playerTwoScore.innerText = score__1
+        playerOneSection.classList.add("player--active")
+        playerTwoSection.classList.remove("player--active")
+    }
+}
 
 holdBtn.addEventListener("click", function(){
-    if(current_user=== "player-1"){
-        localStorage.setItem("current-player", "player-2")
-        playerTwo.classList.add("player--active")
-        playerOne.classList.remove("player--active")
-    }else{
-        localStorage.setItem("current-player", "player-1")
-        playerOne.classList.add("player--active")
-        playerTwo.classList.remove("player--active")
+    ChangeActivePlayer()
+})
 
 
-    }
-    
+// ================== NEW GAME BUTTON EVENT LISTENER =======================
+
+newGame.addEventListener("click", function(){
+    window.location.reload()
 })
 
 
